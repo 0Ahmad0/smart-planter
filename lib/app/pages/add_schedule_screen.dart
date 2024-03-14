4be7bda199024849b71f0ace1f0162039c8e103a
Ahmad Alhariri‏ <0ahmad.haboosh0@gmail.com>
2024-03-14 10:41:47 +0300

    done add schedule Bro

--- lib/app/pages/add_schedule_screen.dart
+++ lib/app/pages/add_schedule_screen.dart
@@ -105,8 +105,7 @@ class _AddScheduleScreenState extends State<AddScheduleScreen> {
             child: Padding(
               padding: const EdgeInsets.all(AppPadding.p12),
               child: TextFiledApp(
-                iconDataImage: AssetsManager.addNewPlantIMG,
-                // iconDataImage: AssetsManager.clockIMG,
+                iconDataImage: AssetsManager.clockIMG,
                 hintText: 'Am Time',
                 onTap: () async {
                   showModalBottomSheet(
@@ -185,8 +184,7 @@ class _AddScheduleScreenState extends State<AddScheduleScreen> {
                   Padding(
                     padding: const EdgeInsets.all(AppPadding.p12),
                     child: TextFiledApp(
-                      iconDataImage: AssetsManager.addNewPlantIMG,
-                      // iconDataImage: AssetsManager.clockIMG,
+                      iconDataImage: AssetsManager.clockIMG,
                       hintText: 'Pm Time',
                       onTap: () async {
                         showModalBottomSheet(
@@ -249,8 +247,7 @@ class _AddScheduleScreenState extends State<AddScheduleScreen> {
                   Padding(
                     padding: const EdgeInsets.all(AppPadding.p12),
                     child: TextFiledApp(
-                      iconDataImage: AssetsManager.addNewPlantIMG,
-                      // iconDataImage: AssetsManager.clockIMG,
+                      iconDataImage: AssetsManager.clockIMG,
                       hintText: 'interval Time',
                       onTap: () async {
                         showModalBottomSheet(
